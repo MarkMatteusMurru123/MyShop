100d6fd5d79193b1d1259d7fa620b039e4c05db4
Mark Matteus Murru <mamurr@ttu.ee>
2020-10-22 16:39:07 +0300

    Add project files.

---
+++ Soft/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Soft.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<OrderData> Orders { get; set; } //siia paned mitmused! - need on orderite setid
+        public DbSet<OrderItemData> OrderItems { get; set; }
+        public DbSet<CatalogTypeData> CatalogTypes { get; set; }
+        public DbSet<CatalogBrandData> CatalogBrands { get; set; }
+        public DbSet<CatalogData> Catalogs { get; set; }
+        public DbSet<BasketData> Baskets { get; set; }
+        public DbSet<BasketItemData> BasketItems { get; set; }
+
+
+
+
+
+
+    }
+}
